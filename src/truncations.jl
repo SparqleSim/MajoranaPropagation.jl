9d3bf9853299365707efec11e9278815e3f0a1c1
MatteDAnna <mattedanna@gmail.com>
2026-02-16 17:08:47 +0100

    new example notebook on Trotter consistent cutting

--- src/truncations.jl
+++ src/truncations.jl
@@ -82,3 +82,39 @@ function PropagationBase.truncate!(
 
     return
 end
+
+function PropagationBase.truncate!(
+    msum::AbstractMajoranaSum;
+    max_weight::Real=Inf, min_abs_coeff=1e-10, max_unpaired::Real=Inf,
+    max_freq::Real=Inf, max_sins::Real=Inf,
+    unpaired_mask=nothing,
+    customtruncfunc=nothing,
+    kwargs...
+)
+    if isnothing(unpaired_mask)
+        unpaired_mask = create_unpaired_mask(nfermions(msum))
+    end
+    function truncfunc(mstr, coeff)
+        # slight customization of the truncation function 
+        # to truncate majorana weight and single
+        is_truncated = false
+        if PauliPropagation.truncatemincoeff(coeff, min_abs_coeff)
+            is_truncated = true
+        elseif truncateunpaired(mstr, max_unpaired, unpaired_mask)
+            is_truncated = true
+        elseif truncatemajoranaweight(mstr, max_weight)
+            is_truncated = true
+        elseif PauliPropagation.truncatefrequency(coeff, max_freq)
+            is_truncated = true
+        elseif PauliPropagation.truncatesins(coeff, max_sins)
+            is_truncated = true
+        elseif !isnothing(customtruncfunc) && customtruncfunc(mstr, coeff)
+            is_truncated = true
+        end
+
+        return is_truncated
+    end
+    msum = truncate!(truncfunc, msum; kwargs...)
+
+    return msum
+end
