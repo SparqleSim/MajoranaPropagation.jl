4ffbeaf7c76fc6e819ebe2c816d9b433a71f5065
MatteDAnna <mattedanna@gmail.com>
2026-05-14 19:09:17 +0200

    added truncations for Sum itself

--- src/multidict/truncations.jl
+++ src/multidict/truncations.jl
@@ -13,13 +13,13 @@ function PropagationBase.truncate!(
         is_truncated = false
         if PauliPropagation.truncatemincoeff(coeff, min_abs_coeff)
             is_truncated = true
-        elseif truncateunpaired(mstr, max_unpaired, unpaired_mask)
+        elseif isfinite(max_unpaired) && truncateunpaired(mstr, max_unpaired, unpaired_mask)
             is_truncated = true
-        elseif truncatemajoranaweight(mstr, max_weight)
+        elseif isfinite(max_weight) && truncatemajoranaweight(mstr, max_weight)
             is_truncated = true
-        elseif PauliPropagation.truncatefrequency(coeff, max_freq)
+        elseif isfinite(max_freq) && PauliPropagation.truncatefrequency(coeff, max_freq)
             is_truncated = true
-        elseif PauliPropagation.truncatesins(coeff, max_sins)
+        elseif isfinite(max_sins) && PauliPropagation.truncatesins(coeff, max_sins)
             is_truncated = true
         elseif !isnothing(customtruncfunc) && customtruncfunc(mstr, coeff)
             is_truncated = true
@@ -34,6 +34,43 @@ function PropagationBase.truncate!(
     end
 
     setmainsum!(prop_cache, msum)
+
+    return
+end
+
+function PropagationBase.truncate!(
+    msum::MajoranaSumMulti;
+    max_weight::Real=Inf, min_abs_coeff=1e-10, max_unpaired::Real=Inf,
+    max_freq::Real=Inf, max_sins::Real=Inf,
+    unpaired_mask=nothing,
+    customtruncfunc=nothing,
+    kwargs...
+)
+    if isnothing(unpaired_mask)
+        unpaired_mask = create_unpaired_mask(nfermions(msum))
+    end
+    function truncfunc(mstr, coeff)
+        is_truncated = false
+        if PauliPropagation.truncatemincoeff(coeff, min_abs_coeff)
+            is_truncated = true
+        elseif isfinite(max_unpaired) && truncateunpaired(mstr, max_unpaired, unpaired_mask)
+            is_truncated = true
+        elseif isfinite(max_weight) && truncatemajoranaweight(mstr, max_weight)
+            is_truncated = true
+        elseif isfinite(max_freq) && PauliPropagation.truncatefrequency(coeff, max_freq)
+            is_truncated = true
+        elseif isfinite(max_sins) && PauliPropagation.truncatesins(coeff, max_sins)
+            is_truncated = true
+        elseif !isnothing(customtruncfunc) && customtruncfunc(mstr, coeff)
+            is_truncated = true
+        end
+
+        return is_truncated
+    end
+
+    @threads for iw in eachindex(msum)
+        _truncate_dict!(msum.MultiMajoranas[iw], truncfunc)
+    end
 
     return
 end
