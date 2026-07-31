297eb09a76eea703ebd9f48cc0b163919cd41ef4
MatteDAnna <mattedanna@gmail.com>
2026-07-31 12:41:29 +0200

    fixed some bugs, extended tests

--- src/FermionToQubitMapping/JordanWigner.jl
+++ src/FermionToQubitMapping/JordanWigner.jl
@@ -3,13 +3,11 @@ using MajoranaPropagation
 using PauliPropagation
 
 function JordanWigner(msum::MajoranaSum{TT,CT}) where {TT<:Integer,CT}
-    @assert msum.is_spinful == false
     n_fermions = MajoranaPropagation.nfermions(msum)
 
     psum = PauliSum(CT, n_fermions)
     for (ms, coeff) in msum
         mstr_jw, phase = JordanWigner(ms, msum.nsites, msum.is_spinful)
-        #@show bitstring(mstr_jw), phase
         add!(psum, mstr_jw, coeff * phase)
     end
     return psum
@@ -19,20 +17,15 @@ function JordanWigner(mstr::TT, n_sites::Int, is_spinful::Bool) where {TT<:Integ
     n_fermions = is_spinful ? 2 * n_sites : n_sites
     mstr_jw = TT(0)
     phase = (1im)^omega_L_mult(mstr)
-    #@show bitstring(mstr)
     for i = 1:n_fermions
-        #println("------")
         gammas = TT(getpauli(mstr, i))
-        #@show i, bitstring(gammas)
         if gammas == 1 || gammas == 2
             if i > 1
                 jw_string = PauliString(n_fermions, repeat([:Z], i - 1), 1:i-1).term
             else
                 jw_string = TT(0)
             end
-            #@show bitstring(jw_string)
             gammas = jw_string | (gammas << (2 * (i - 1)))
-            #@show bitstring(gammas)
         elseif gammas == 3
             gammas = gammas << (2 * (i - 1))
             phase *= 1im
@@ -59,12 +52,12 @@ end
 function JordanWigner(n_sites, is_spinful, circ::Vector{FermionicRotation}, thetas::Vector{CT}) where {CT}
     pp_circ = PauliRotation[]
     pp_thetas = CT[]
+    n_fermions = is_spinful ? 2 * n_sites : n_sites
     for (gate, theta) in zip(circ, thetas)
         ms_rotations, coeffs, _ = MajoranaPropagation.getmajoranarotations(gate, n_sites)
         for (ms_rotation, coeff) in zip(ms_rotations, coeffs)
-            # TODO: buggy if spinful, FIX asap
             ms_jw, phase = JordanWigner(ms_rotation.ms_int, n_sites, is_spinful)
-            push!(pp_circ, _make_gate(ms_jw, n_sites))
+            push!(pp_circ, _make_gate(ms_jw, n_fermions))
             # factor 2 because `FermionicRotation` applies each `MajoranaRotation` with angle 2 * coeff * theta,
             # matching `PauliRotation`'s exp(-i * theta/2 * pstr) convention
             push!(pp_thetas, 2.0 * coeff * theta * phase)
@@ -73,26 +66,12 @@ function JordanWigner(n_sites, is_spinful, circ::Vector{FermionicRotation}, thet
     return pp_circ, pp_thetas
 end
 
-# PauliPropagation >= 0.8 defines `paulipropagation2yao` itself; with older versions
-# it only exists in YaoBlocks' PauliPropagationExt, so resolve it at call time
-function _paulipropagation2yao(args...)
-    if isdefined(PauliPropagation, :paulipropagation2yao)
-        return PauliPropagation.paulipropagation2yao(args...)
-    end
-    for (id, mod) in Base.loaded_modules
-        if id.name == "YaoBlocks" && isdefined(mod, :paulipropagation2yao)
-            return mod.paulipropagation2yao(args...)
-        end
-    end
-    error("`paulipropagation2yao` not found. Load Yao (`using Yao`) or use PauliPropagation >= 0.8.")
-end
-
 function majoranapropagation2yao(n_sites, is_spinful, circ, thetas)
     nqubits = is_spinful ? 2 * n_sites : n_sites
     pp_circ, pp_thetas = JordanWigner(n_sites, is_spinful, circ, thetas)
-    return _paulipropagation2yao(nqubits, pp_circ, pp_thetas)
+    return paulipropagation2yao(nqubits, pp_circ, pp_thetas)
 end
 
 function majoranapropagation2yao(msum::MajoranaSum)
-    return _paulipropagation2yao(JordanWigner(msum))
+    return paulipropagation2yao(JordanWigner(msum))
 end
--- test/compare_jw.jl
+++ test/compare_jw.jl
@@ -16,7 +16,7 @@ using Test
             obs = (:n, 4)
             msum = MajoranaSum(n_fermions, obs[1], obs[2])
             msum_pp = JordanWigner(msum)
-            yao_obs = kron(n_fermions, obs[2] => Z)
+            yao_obs = majoranapropagation2yao(msum)
 
             #initial state
             occupied_sites = 1:2:n_fermions
@@ -39,7 +39,7 @@ using Test
 
             mp_res = overlapwithfock(msum, fock_state)
             pp_res = overlapwithcomputational(msum_pp, occupied_sites)
-            yao_res = (1. - Yao.expect(yao_obs, yao_psi)) / 2. # convert from Z expectation to occupation number
+            yao_res = Yao.expect(yao_obs, yao_psi)
             @test abs(mp_res - yao_res) < 1.e-12
             @test abs(pp_res - yao_res) < 1.e-12
 
@@ -51,7 +51,7 @@ using Test
 
                 mp_res = overlapwithfock(msum, fock_state)
                 pp_res = overlapwithcomputational(msum_pp, occupied_sites)
-                yao_res = (1. - Yao.expect(yao_obs, yao_psi)) / 2. # convert from Z expectation to occupation number
+                yao_res = Yao.expect(yao_obs, yao_psi)
                 @test abs(mp_res - yao_res) < 1.e-12
                 @test abs(pp_res - yao_res) < 1.e-12
             end
@@ -68,9 +68,7 @@ using Test
             msum = MajoranaSum(n_fermions, obs[1], obs[2])
             msum_pp = JordanWigner(msum)
 
-            yao_Z_s1 = kron(n_fermions, obs[2][1] => Z)
-            yao_Z_s2 = kron(n_fermions, obs[2][2] => Z)
-            yao_ZZ = kron(n_fermions, site => Z for site in obs[2])
+            yao_obs = majoranapropagation2yao(msum)
 
             #initial state
             occupied_sites = 2:2:n_fermions
@@ -104,7 +102,121 @@ using Test
 
                 mp_res = overlapwithfock(msum, fock_state)
                 pp_res = overlapwithcomputational(msum_pp, occupied_sites)
-                yao_res = (1. - Yao.expect(yao_Z_s1, yao_psi) - Yao.expect(yao_Z_s2, yao_psi) + Yao.expect(yao_ZZ, yao_psi)) / 4. # convert from Z expectations to occupation number
+                yao_res = Yao.expect(yao_obs, yao_psi)
+
+                @test abs(mp_res - yao_res) < 1.e-12
+                @test abs(pp_res - yao_res) < 1.e-12
+            end
+        end
+    end
+
+    # spinful tests: site i maps to qubits 2i-1 (up) and 2i (down)
+    @testset "spinful" begin
+        @testset "free fermions" begin
+            n_sites = 6
+            is_spinful = true
+            nq = 2 * n_sites
+            h = 0.2
+            topo = bricklayertopology(n_sites)
+
+            obs_site = 3
+            msum = MajoranaSum(n_sites, :nup, obs_site)
+            msum_pp = JordanWigner(msum)
+            yao_obs = majoranapropagation2yao(msum)
+
+            #initial state
+            up_occupied = 1:2:n_sites
+            dn_occupied = 2:2:n_sites
+            fock_state = FockState(n_sites, collect(up_occupied), collect(dn_occupied))
+            occupied_qubits = sort(vcat([2s - 1 for s in up_occupied], [2s for s in dn_occupied]))
+
+            yao_psi = zero_state(nq)
+            state_prep = chain(nq, put(q => Yao.X) for q in occupied_qubits)
+            Yao.apply!(yao_psi, state_prep)
+
+            #build circuits
+            circ::Vector{FermionicRotation} = []
+            thetas = []
+            for (i, j) in topo
+                push!(circ, FermionicRotation(:hopup, [i, j]))
+                push!(thetas, h)
+                push!(circ, FermionicRotation(:hopdn, [i, j]))
+                push!(thetas, h)
+            end
+
+            pp_circ, pp_thetas = JordanWigner(n_sites, is_spinful, circ, thetas)
+            yao_circ = majoranapropagation2yao(n_sites, is_spinful, circ, thetas)
+
+            mp_res = overlapwithfock(msum, fock_state)
+            pp_res = overlapwithcomputational(msum_pp, occupied_qubits)
+            yao_res = Yao.expect(yao_obs, yao_psi)
+            @test abs(mp_res - yao_res) < 1.e-12
+            @test abs(pp_res - yao_res) < 1.e-12
+
+            n_iters = 200
+            for _ in 1:n_iters
+                propagate!(circ, msum, thetas; min_abs_coeff=-1.)
+                propagate!(pp_circ, msum_pp, pp_thetas; min_abs_coeff=-1.)
+                Yao.apply!(yao_psi, yao_circ)
+
+                mp_res = overlapwithfock(msum, fock_state)
+                pp_res = overlapwithcomputational(msum_pp, occupied_qubits)
+                yao_res = Yao.expect(yao_obs, yao_psi)
+                @test abs(mp_res - yao_res) < 1.e-12
+                @test abs(pp_res - yao_res) < 1.e-12
+            end
+        end
+
+        @testset "Hubbard" begin
+            n_sites = 5
+            is_spinful = true
+            nq = 2 * n_sites
+            U = 0.5
+            h = 0.2
+            topo = bricklayertopology(n_sites)
+
+            obs_site = 3
+            msum = MajoranaSum(n_sites, :nupndn, obs_site)
+            msum_pp = JordanWigner(msum)
+
+            yao_obs = majoranapropagation2yao(msum)
+
+            #initial state
+            up_occupied = 1:2:n_sites
+            dn_occupied = 2:2:n_sites
+            fock_state = FockState(n_sites, collect(up_occupied), collect(dn_occupied))
+            occupied_qubits = sort(vcat([2s - 1 for s in up_occupied], [2s for s in dn_occupied]))
+
+            yao_psi = zero_state(nq)
+            state_prep = chain(nq, put(q => Yao.X) for q in occupied_qubits)
+            Yao.apply!(yao_psi, state_prep)
+
+            #build circuits
+            circ::Vector{FermionicRotation} = []
+            thetas = []
+            for (i, j) in topo
+                push!(circ, FermionicRotation(:hopup, [i, j]))
+                push!(thetas, h)
+                push!(circ, FermionicRotation(:hopdn, [i, j]))
+                push!(thetas, h)
+            end
+            for i = 1:n_sites
+                push!(circ, FermionicRotation(:nupndn, i))
+                push!(thetas, U)
+            end
+
+            pp_circ, pp_thetas = JordanWigner(n_sites, is_spinful, circ, thetas)
+            yao_circ = majoranapropagation2yao(n_sites, is_spinful, circ, thetas)
+
+            n_iters = 5
+            for _ in 1:n_iters
+                propagate!(circ, msum, thetas; min_abs_coeff=1.e-14)
+                propagate!(pp_circ, msum_pp, pp_thetas; min_abs_coeff=1.e-14)
+                Yao.apply!(yao_psi, yao_circ)
+
+                mp_res = overlapwithfock(msum, fock_state)
+                pp_res = overlapwithcomputational(msum_pp, occupied_qubits)
+                yao_res = Yao.expect(yao_obs, yao_psi)
 
                 @test abs(mp_res - yao_res) < 1.e-12
                 @test abs(pp_res - yao_res) < 1.e-12
